f478bc5d8643e5d283b31f215265405208388b08
Josy Li <josyliyh@gmail.com>
2023-09-05 15:09:48 -0700

    output about page acf

--- page-about.php
+++ page-about.php
@@ -16,12 +16,82 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+		
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
+
+			if ( function_exists('get_field') ) :
+
+				if ( get_field('about_banner_image')) :
+					$image = get_field('about_banner_image');
+					$size = 'large'; // (thumbnail, medium, large, full or custom size)
+					if( $image ) {
+						?>
+							<img class="about-image"><?php
+							echo wp_get_attachment_image( $image, $size );
+							?>
+						<?php
+					}
+				endif;
+
+				if ( get_field('about_message')) :
+					?>
+					<p class="about-message"><?php echo the_field('about_message'); ?></p>
+					<?php
+				endif;
+
+				if ( get_field('company_info_title')) :
+					?>
+					<h2><?php echo the_field('company_info_title'); ?></h2>
+					<?php
+				endif;
+
+				if ( get_field('company_name')) :
+					?>
+					<h1><?php echo the_field('company_name'); ?></h1>
+					<?php
+				endif;
+
+				if ( get_field('company_address')) :
+					?>
+					<p><?php echo the_field('company_address'); ?></p>
+					<?php
+				endif;
+
+				if ( get_field('company_ceo')) :
+					?>
+					<p><?php echo the_field('company_ceo'); ?></p>
+					<?php
+				endif;
+
+				if ( get_field('company_founded')) :
+					?>
+					<p><?php echo the_field('company_founded'); ?></p>
+					<?php
+				endif;
+
+				if ( get_field('ecsite_image')) :
+					$image = get_field('ecsite_image');
+					$size = 'large'; // (thumbnail, medium, large, full or custom size)
+					if( $image ) {
+						?>
+							<img class="about-image"><?php
+							echo wp_get_attachment_image( $image, $size );
+							?>
+						<?php
+					}
+				endif;
+
+
+				if ( get_field('ecsite_message')) :
+					?>
+					<p><?php echo the_field('ecsite_message'); ?></p>
+					<?php
+				endif;
+			endif;
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
